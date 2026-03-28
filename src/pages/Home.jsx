393fde5894b1e64642662ac5dad9aa7660aeaa95
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2026-03-28 08:51:45 +0100

    more component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Hero from "../components/Hero";
 import VisionMission from "../components/VisionMission";
+import WeeklyStats from "../components/WeeklyStats";
+import WhyChooseUs from "../components/WhyChooseUs";
+import HowItWorks from "../components/HowItWorks";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <VisionMission />
+      <WeeklyStats />
+      <WhyChooseUs />
+      <HowItWorks />
     </div>
   );
 }
