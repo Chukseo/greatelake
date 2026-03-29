f151ad3ca4f9a8caebefd56607f76bdd84b5ce97
Chuks Emmanuel Okolo <164390660+Chukseo@users.noreply.github.com>
2026-03-29 17:58:36 +0100

    about page2

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,13 @@
 import AboutHero from "../components/AboutHero";
 import Mission from "../components/Mission";
+import Wcu from "../components/Wcu";
 
 export default function AboutUs() {
   return (
     <div>
       <AboutHero />
       <Mission />
+      <Wcu />
     </div>
   );
 }
